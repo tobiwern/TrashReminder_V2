5b375a1753064fd5066c8682a827ade52ea09b13
tobiwern <tobiwern@gmail.com>
2024-01-05 14:04:01 +0100

    Name changes for TrashReminder_V2

--- webpage.h
+++ webpage.h
@@ -1,22 +1,22 @@
 // don't add taskId if the number is already in the list (if usr selects two ics files with identical content)
-//How to reference a file from a branch https://raw.githubusercontent.com/tobiwern/TrashReminder/<Branch>/TrashReminder.ino
-//How to reference a md from a branch https://github.com/tobiwern/TrashReminder/blob/<Branch>/README.md (however this is viewed along with the code...)
+//How to reference a file from a branch https://raw.githubusercontent.com/tobiwern/TrashReminder_V2/<Branch>/TrashReminder_V2.ino
+//How to reference a md from a branch https://github.com/tobiwern/TrashReminder_V2/blob/<Branch>/README.md (however this is viewed along with the code...)
 const char webpage[] PROGMEM = R"=====(
 <!DOCTYPE html>
   <html lang="de">
     <head>
       <meta charset='utf-8'>
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Müll-Erinnerung</title>
+      <title>Müll-Erinnerung (Version 2)</title>
       <meta name="robots" content="noindex">
       <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
       <meta http-equiv="Pragma" content="no-cache" />
       <meta http-equiv="Expires" content="0" />
       <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-      <link rel="stylesheet" href="https://tobiwern.github.io/TrashReminder/colorPicker/colorPick.css">
-      <script src="https://tobiwern.github.io/TrashReminder/colorPicker/colorPick.js"></script>
-      <link rel="stylesheet" href="https://tobiwern.github.io/TrashReminder/TrashReminder.css">
-      <script src="https://tobiwern.github.io/TrashReminder/TrashReminder.js">"></script> 
+      <link rel="stylesheet" href="https://tobiwern.github.io/TrashReminder_V2/colorPicker/colorPick.css">
+      <script src="https://tobiwern.github.io/TrashReminder_V2/colorPicker/colorPick.js"></script>
+      <link rel="stylesheet" href="https://tobiwern.github.io/TrashReminder_V2/TrashReminder.css">
+      <script src="https://tobiwern.github.io/TrashReminder_V2/TrashReminder.js">"></script> 
     </head>  
     <body id='body' onload='createWebpage();requestSettingsFromESP();requestTasksFromESP(false);')>    
     </body>

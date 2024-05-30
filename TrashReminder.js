2b1d34b8c7bead80e34df479ab5ab2425ed5a6b8
tobiwern <tobiwern@gmail.com>
2024-05-30 20:54:24 +0200

    Update TrashReminder.js

--- TrashReminder.js
+++ TrashReminder.js
@@ -304,7 +304,7 @@ function promptForTaskTypeCombine(){
   }
 
   if(duplicates.length > 0){ //means combining entries
-    const response = confirm("Willst Du " + mismatches[0] + " unter " + duplicates[0] + " zusammenführen?");
+    const response = confirm("Willst Du \"" + mismatches[0] + "\" unter dem neuen Namen \"" + duplicates[0] + "\" zusammenführen?");
     if(response){
       return([mismatches[0],duplicates[0]]);
     } else {

914114403184e10f168dce6b4ba3d8f6f6af74d7
tobiwern <tobiwern@gmail.com>
2024-12-19 10:04:39 +0100

    Fixed bug with reset validtaskIds when taskDescription is changed

--- TrashReminder.js
+++ TrashReminder.js
@@ -324,7 +324,7 @@ function sendTaskDescriptionToESP() {
   var tasks = [];
   var colors = [];
   var renameIds = {};
-  var validTaskIds = [];
+//  var validTaskIds = [];
   console.log("Before: gDataTasks = " + JSON.stringify(gDataTasks));
   const taskTypes = document.getElementsByClassName("taskType_desc");
   for (let i = 0; i < taskTypes.length; i++) {
@@ -364,14 +364,15 @@ function sendTaskDescriptionToESP() {
     }
   }
   console.log("After: gDataEpochTaskDict = " + JSON.stringify(gDataEpochTaskDict));
-  //correct gDataValidTasks
-  
+
+  //correct gDataValidTasks 
 //  for(taskId of getValidTaskIds){
 //    if(taskId < tasks.length){
 //      validTaskIds.push(taskId);
 //    }
 //  }
-  gDataValidTaskIds = validTaskIds;
+
+//  gDataValidTaskIds = validTaskIds;
   gDataTasks = tasks;  
   gDataColors = colors;
   refreshTaskTypes();
@@ -540,7 +541,6 @@ function refreshTaskTypes() {
         checked = (gDataValidTaskIds.length == 0 || gDataValidTaskIds.includes(i)) ? "checked" : "";
         text += "<tr>"
         text += "<td class='checkbox'><input type='checkbox' class='taskType' onChange='refreshTaskDates();sendValidTaskTypesToESP();' id='taskType" + i + "' name=task" + i + "' " + checked + "></td>";
-//        text += "<label for='taskType" + i + "' id='taskTypel" + i + "'> " + gDataTasks[i] + "</label><div></td>";
         text += "<td><input type='text' class='taskType_desc' id='taskType_desc" + i + "' value='" + gDataTasks[i] + "' size='30' onfocusout='sendTaskDescriptionToESP();refreshTaskDates();'></td>";
         text += "<td><div class='colorPickSelector' id='colorPickerTask" + i + "'></div></td>";
         text += "</tr>";
